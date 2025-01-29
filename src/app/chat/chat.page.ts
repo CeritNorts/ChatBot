5d82356d78b6c9bd9d82b3e77be96462e554f07f
CeritNorts <20223l001058@utcv.edu.mx>
2025-01-28 22:59:28 -0600

    Agregando mas mesjaes al ngFor

--- src/app/chat/chat.page.ts
+++ src/app/chat/chat.page.ts
@@ -8,13 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ChatPage implements OnInit {
-
+  //Generamos la lista de mensajes que se utilizará en el chat por medio de *ngFor
   messages = [
     { username: 'Emmanuel', message: 'Dice mi mamá que pongas papa a serenar para la alopecia', date: '01:28', avatarURL: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { username: 'Carlos', message: '¿En serio funciona eso?', date: '01:30', avatarURL: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { username: 'Ana', message: 'Jajaja, quiero intentarlo.', date: '01:32', avatarURL: 'https://ionicframework.com/docs/img/demos/avatar.svg' }
+    { username: 'Pablo', message: 'Yes bro', date: '01:30', avatarURL: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
+    { username: 'Oscar', message: 'Eso no es cierto pa', date: '01:32', avatarURL: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
+    { username: 'Eliezer', message: 'okas okas', date: '01:32', avatarURL: 'https://ionicframework.com/docs/img/demos/avatar.svg' }
   ];
-  
+
   constructor() { }
 
   ngOnInit() {

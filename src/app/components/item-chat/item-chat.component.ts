000de8310f09e78025bbfef691441aeef4fb0c8c
CeritNorts <20223l001058@utcv.edu.mx>
2025-01-28 21:37:45 -0600

    ChatBot Generado en tabs

---
+++ src/app/components/item-chat/item-chat.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-chat',
+  templateUrl: './item-chat.component.html',
+  styleUrls: ['./item-chat.component.scss'],
+  standalone: false,
+})
+export class ItemChatComponent  implements OnInit {
+
+  @Input() username: string = '';
+  @Input() massage: string = '';
+  @Input() date: string = '';
+  @Input() avatarURL: string = '';
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}

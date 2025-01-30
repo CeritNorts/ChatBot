5e34d4dd68355ad700b95f007a8ca06d58b1525e
CeritNorts <20223l001058@utcv.edu.mx>
2025-01-29 20:52:56 -0600

    Pasando el chat a tabs

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,6 +8,15 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  //Generamos la lista de mensajes que se utilizará en el chat por medio de *ngFor
+  chats = [
+    { username: 'Emmanuel', message: 'Dice mi mamá que pongas papa a serenar para la alopecia', date: '01:28', avatarURL: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
+    { username: 'Pablo', message: 'Yes bro', date: '01:30', avatarURL: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
+    { username: 'Oscar', message: 'Eso no es cierto pa', date: '01:32', avatarURL: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
+    { username: 'Eliezer', message: 'okas okas', date: '01:32', avatarURL: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
+    { username: 'Tano', message: 'Si bro', date: '01:32', avatarURL: 'https://ionicframework.com/docs/img/demos/avatar.svg' }
+  ];
+
   constructor() {}
 
 }

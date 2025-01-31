adaac4c4e5598cb82291656f319f996b41f43bf7
CeritNorts <20223l001058@utcv.edu.mx>
2025-01-30 19:06:31 -0600

    inicio de los estados en el chatBot

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ItemChatComponent } from './item-chat/item-chat.component';
 import { IonicModule } from '@ionic/angular';
+import { ItemEstadosComponent } from './item-estados/item-estados.component';
 
 
 
 @NgModule({
   declarations: [
-    ItemChatComponent
+    ItemChatComponent,
+    ItemEstadosComponent
   ],
   imports: [
     IonicModule,
     CommonModule
   ],
   exports: [	
-    ItemChatComponent
+    ItemChatComponent,
+    ItemEstadosComponent
   ]
 })
 export class ComponentsModule { }
---
+++ src/app/components/item-estados/item-estados.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-estados',
+  templateUrl: './item-estados.component.html',
+  styleUrls: ['./item-estados.component.scss'],
+  standalone: false,
+})
+export class ItemEstadosComponent  implements OnInit {
+
+  @Input() username: string = '';
+  @Input() date: string = '';
+  @Input() avatarURL: string = '';
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
